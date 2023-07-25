2836f152b4e94b49eb1003cbcc065302d8160100
小锋 <984904208@qq.com>
2023-07-25 20:20:49 +0800

    添加发布页标题栏

--- src/pages/PostEditor/index.js
+++ src/pages/PostEditor/index.js
@@ -2,13 +2,37 @@
 import React, { useState } from 'react';
 import { MdEditor } from 'md-editor-rt';
 import styled from 'styled-components';
+import { Input, Button, Avatar } from 'antd';
 
 const PostEditorStyle = styled.div`
-  .md-editor-dark {
-    --md-bk-color: #333 !important;
-  }
   div#md-editor-rt {
     height: 100vh;
+  }
+  .post_editor_head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 27px;
+    height: 60px;
+    input {
+      width: 600px;
+      margin: 0;
+      padding: 0;
+      font-size: 24px;
+      font-weight: 500;
+      color: #1d2129;
+      border: 0;
+      border-style:none; 
+      outline: none;
+      box-shadow: none;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0);
+    }
+    .post_editor_right {
+      button {
+        margin: 0 8px;
+      }
+    }
   }
 `
 
@@ -17,6 +41,13 @@ const PostEditor = () => {
 
   return (
     <PostEditorStyle>
+      <div className='post_editor_head'>
+        <Input placeholder="请输入标题..." />
+        <div className='post_editor_right'>
+          <Button type="primary">发布</Button>
+          <Avatar size={32} icon="朝阳" />
+        </div>
+      </div>
       <MdEditor modelValue={text} onChange={setText}/>
     </PostEditorStyle>
   );
